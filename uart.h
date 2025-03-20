119d9633f79a4ba232b8410608d08746564ca8b9
Zorohmmm <donamkhanhyt2903@gmail.com>
2025-03-20 18:42:37 +0700

    Create uart.h

---
+++ uart.h
@@ -0,0 +1,40 @@
+
+#include "stm32f10x.h"
+
+
+
+void UART_Init(void) {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    USART_InitTypeDef USART_InitStruct;
+
+    // TX - PA9
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // RX - PA10
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    USART_InitStruct.USART_BaudRate = 9600;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_Init(USART1, &USART_InitStruct);
+
+    USART_Cmd(USART1, ENABLE);
+}
+
+void UART_Send_String(char *str) {
+    while (*str) {
+        USART_SendData(USART1, *str++);
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+    }
+}
